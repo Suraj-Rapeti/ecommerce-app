03b375f046d98c106f462973205d340fa214645b
Suraj Rapeti <suraj.rapeti06@gmail.com>
2026-03-21 00:31:08 +0530

    security: move firebase config to environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,9 +6,9 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: "ecommerce-app-756a2.firebasestorage.app",
-  messagingSenderId: "306357383449",
-  appId: "1:306357383449:web:4fd0ef0e176c9cb3df5588"
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 
